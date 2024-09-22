852bb9dc199bcf75db33bfa396f28e7f37d50726
YugantGotmare <yugantgotmare123@gmail.com>
2024-09-22 22:04:18 +0530

    Get_Embeddings

---
+++ get_embeddings.py
@@ -0,0 +1,14 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+def get_embeddings(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vectors = FAISS.from_texts(chunks, embedding=embeddings)
+    vectors.save_local("faiss_index")
