fcab08ee93942518fc36c0abc1302b20ab011803
YugantGotmare <yugantgotmare123@gmail.com>
2024-09-22 22:05:42 +0530

    Get_text_chunks

---
+++ get_text_chunks.py
@@ -0,0 +1,14 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
